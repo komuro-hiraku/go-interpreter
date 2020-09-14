b24a18ee5f881080971bf71b6a0c2886be5f01e1
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-09-15 00:15:09 +0900

    - feat: Add Return Statement (WIP)

--- ast/ast.go
+++ ast/ast.go
@@ -33,19 +33,29 @@ func (p *Program) TokenLiteral() string {
 
 // Let ノード
 type LetStatement struct {
-	Token token.Token	// token.LET トークン
-	Name *Identifier
-	Value Expression	
+	Token token.Token // token.LET トークン
+	Name  *Identifier
+	Value Expression
 }
 
 // Statement, Node インタフェースを満たすための実装
-func (ls *LetStatement) statementNode() {}
+func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+// Return ノード
+type ReturnStatement struct {
+	Token       token.Token // 'return' トークン
+	ReturnValue Expression
+}
+
+// Statement, Node インタフェースを満たすための実装
+func (rt *ReturnStatement) statementNode()       {}
+func (rt *ReturnStatement) TokenLiteral() string { return rt.Token.Literal }
+
 type Identifier struct {
-	Token token.Token	// token.IDENT トークン
+	Token token.Token // token.IDENT トークン
 	Value string
 }
 
-func (i *Identifier) expressionNode() {}
+func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
