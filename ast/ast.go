e73c776e5ae02ba4d11d9ac1d76636598d337f4c
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-09-15 23:03:18 +0900

    - Add String() interface to Node type

--- ast/ast.go
+++ ast/ast.go
@@ -1,11 +1,13 @@
 package ast
 
 import (
+	"bytes"
 	"github.com/komuro-hiraku/monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -31,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	// Statements の中身をBufferに詰めて文字列にする
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 // Let ノード
 type LetStatement struct {
 	Token token.Token // token.LET トークン
@@ -38,9 +50,22 @@ type LetStatement struct {
 	Value Expression
 }
 
-// Statement, Node インタフェースを満たすための実装
+// Let: Statement, Node インタフェースを満たすための実装
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 // Return ノード
 type ReturnStatement struct {
@@ -48,10 +73,22 @@ type ReturnStatement struct {
 	ReturnValue Expression
 }
 
-// Statement, Node インタフェースを満たすための実装
+// Return: Statement, Node インタフェースを満たすための実装
 func (rt *ReturnStatement) statementNode()       {}
 func (rt *ReturnStatement) TokenLiteral() string { return rt.Token.Literal }
+func (rt *ReturnStatement) String() string {
+	var out bytes.Buffer
 
+	out.WriteString(rt.TokenLiteral() + " ")
+
+	if rt.ReturnValue != nil {
+		out.WriteString(rt.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+// Identifier ノード
 type Identifier struct {
 	Token token.Token // token.IDENT トークン
 	Value string
@@ -59,3 +96,21 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string {
+	return i.Value
+}
+
+// Expression ノード
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (ex *ExpressionStatement) expressionNode() {}
+func (ex *ExpressionStatement) TokenLiteral() string { return ex.Token.Literal }
+func (ex *ExpressionStatement) String() string {
+	if ex.Expression != nil {
+		return ex.Expression.String()
+	}
+	return ""
+}
