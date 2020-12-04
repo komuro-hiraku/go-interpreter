9157cfb0d2e6a3b46bc8c2202b08b78ebde3822a
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-12-04 23:36:28 +0900

    - envの初期化位置を間違えていた。そりゃforで毎度リセットされてたら保存されねーわ

--- repl/repl.go
+++ repl/repl.go
@@ -15,7 +15,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-
+	env := object.NewEnvironment()
+	
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -28,7 +29,6 @@ func Start(in io.Reader, out io.Writer) {
 		p := parser.New(l)
 
 		program := p.ParseProgram()
-		env := object.NewEnvironment()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
 			continue
