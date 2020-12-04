00097fabc790d06a64ff8f58c91ba924edfead1c
komuro.hiraku <komuro.hiraku@classmethod.jp>
2020-12-04 23:28:30 +0900

    - 一つ先がセミコロンかどうかのチェックを間違っていた

--- parser/parser.go
+++ parser/parser.go
@@ -312,7 +312,7 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	p.nextToken()
 	stmt.Value = p.parseExpression(LOWEST)
 
-	for p.curTokenIs(token.SEMICOLON) {
+	for p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 	return stmt
