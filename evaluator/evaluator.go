073aabcf605b3751da91ddc48f9b81d536ba49b8
komuro.hiraku <komuro.hiraku@classmethod.jp>
2021-01-14 17:27:35 +0900

    - 文字列の結合演算子を追加

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -258,12 +258,24 @@ func evalInfixExperssion(operator string, left, right object.Object) object.Obje
 		return nativeBoolToBooleanObject(left == right)	// 真偽値はStaticなオブジェクトを使っているのでそれらの比較だけで良い
 	case operator == "!=":
 		return nativeBoolToBooleanObject(left != right)
-
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case left.Type() != right.Type():
 		return newError("type mismatch: %s %s %s", left.Type(), operator, right.Type())
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+
+	return &object.String{Value: leftVal + rightVal}
 }
 
 func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
